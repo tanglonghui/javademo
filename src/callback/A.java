c016e5f2e17d3f038c646d432b77925bc23c82ca
tanglonghui <1776234240@qq.com>
2019-05-23 17:31:54 +0800

    修改

--- src/callback/A.java
+++ src/callback/A.java
@@ -3,20 +3,14 @@ package callback;
 public class A {
     public static void main(String[] args) {
         B b = new B();
-        b.setCallBack(new CallBack() {
+        b.Call("充电费",new CallBack() {
             @Override
             public void Notice(String str) {
-                System.out.print("B 的情况"+str);
+                System.out.print("A 收到 B 的消息："+str);
             }
         });
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                b.doSomeThing();
-            }
-        }).start();
         for (int i = 0; i < 10; i++) {
-            System.out.println("A 在做其他的事" + i);
+            System.out.println("A 做其他的事，如看电视" + i);
         }
     }
 }
--- src/callback/B.java
+++ src/callback/B.java
@@ -1,15 +1,35 @@
 package callback;
 
+import java.util.Random;
+
 public class B {
     private CallBack callBack;
-    void doSomeThing(){
-        for (int i=0;i<10;i++){
-            System.out.println("B 正在做事"+i);
+
+    Boolean doSomeThing(String str) {
+        for (int i = 0; i < new Random().nextInt(20); i++) {
+            System.out.println("B 正在执行任务：" + str+"--"+i);
+            if (i>10){
+                System.out.println("B "+str+"失败");
+                return false;
+            }
         }
-        callBack.Notice("完成");
+        System.out.println("B "+str+"成功");
+        return true;
     }
 
-    public void setCallBack(CallBack callBack) {
+    public void Call(String str, CallBack callBack) {
+        System.out.println("B 收到消息：" + str + "，来源" + callBack.toString());
         this.callBack = callBack;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (B.this.doSomeThing(str)) {
+                    callBack.Notice(str + "完成");
+                } else {
+                    callBack.Notice(str + "失败");
+                }
+            }
+        }).start();
+
     }
 }
