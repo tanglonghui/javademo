1695aadb0857b6d04f514198bf08542be7e74309
tanglonghui <1776234240@qq.com>
2019-06-18 17:25:39 +0800

    多态

--- src/extend/Test.java
+++ src/extend/Test.java
@@ -2,7 +2,12 @@ package extend;
 
 public class Test {
     public static void main(String[] args) {
+        //向上转型
         Father son=new Son();
+        //向下转型
+        if (son instanceof Son){
+            Son son1= (Son) son;
+        }
         son.jump();
 
     }
